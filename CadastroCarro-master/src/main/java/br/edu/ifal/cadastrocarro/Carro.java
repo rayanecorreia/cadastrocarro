c52b21fb60fa5e4265054dc38a0c502f97141a59
Luiz Roberto <passageiro1829@gmail.com>
2019-09-17 10:17:21 -0300

    commit inicial

---
+++ CadastroCarro-master/src/main/java/br/edu/ifal/cadastrocarro/Carro.java
@@ -0,0 +1,51 @@
+package br.edu.ifal.cadastrocarro;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Carro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+     private String placa;
+     private String marca;
+     private String modelo;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+     
+}
